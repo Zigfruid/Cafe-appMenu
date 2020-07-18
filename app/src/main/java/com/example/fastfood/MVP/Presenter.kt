ac66842738fcc61cf012adaeb5a612e09745a248
Zigfruid <Zigfruid@gmail.com>
2020-07-18 12:36:19 +0500

    burger was done

--- app/src/main/java/com/example/fastfood/MVP/Presenter.kt
+++ app/src/main/java/com/example/fastfood/MVP/Presenter.kt
@@ -4,12 +4,7 @@ import com.example.fastfood.data.dao.MenuDao
 
 class Presenter(private val dao: MenuDao, private val view : MenuViewHelper) {
 
-    fun getAllMenu(type: Int) {
-        view.fillData(dao.getMenuByType(type))
-    }
-        fun  getMenuFromOrder(){
+    fun  getMenuFromOrder(){
         view.fillData(dao.getAllFromIsOrdered())
         }
-
-
 }
